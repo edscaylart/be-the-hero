dc644ab826d0158fdff7ded9107f7c4f7fbb607a
Edson Souza <ed_f_souza@hotmail.com>
2020-03-27 11:40:59 -0400

    Add statusbar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,11 +2,15 @@ import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
 
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 import Routes from './src/routes';
 
 export default function App() {
   return (
-    <Routes />
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Routes />
+    </>
   );
 }
